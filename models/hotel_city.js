002190e496d6e10c4e01fd662ad418ef52fd31de
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-06-14 21:49:42 +0530

    added some changes

--- models/hotel_city.js
+++ models/hotel_city.js
@@ -17,21 +17,21 @@ const Hotel_by_City= mongoose.model('Hotel_By_City', hSchema);
 
 module.exports = Hotel_by_City;
 // module.exports = Hotel;
-r=async function(){
-    list1=['Goa1','Goa2','Goa3','Goa4','Goa5'];
-    let h=await Hotel_by_City.create({
-        city:'Goa',
-    })
-    for(hotel of list1){
-        await h.hotels.push(hotel);
-    }
+// r=async function(){
+//     list1=['Goa1','Goa2','Goa3','Goa4','Goa5'];
+//     let h=await Hotel_by_City.create({
+//         city:'Goa',
+//     })
+//     for(hotel of list1){
+//         await h.hotels.push(hotel);
+//     }
     
-    list2=['Mumbai1','Mumbai2','Mumbai3','Mumbai4','Mumbai5'];
-    let h1=await Hotel_by_City.create({
-        city:'Mumbai',
-    })
-    for(hotel of list2){
-        await h1.hotels.push(hotel);
-    }
-}
-r();
+//     list2=['Mumbai1','Mumbai2','Mumbai3','Mumbai4','Mumbai5'];
+//     let h1=await Hotel_by_City.create({
+//         city:'Mumbai',
+//     })
+//     for(hotel of list2){
+//         await h1.hotels.push(hotel);
+//     }
+// }
+// r();
